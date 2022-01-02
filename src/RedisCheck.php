228c634250b622e9c720029a8dde25b3129ebf80
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2022-01-02 08:16:16 +0100

    fix(dep): Remove logger dep

--- src/RedisCheck.php
+++ src/RedisCheck.php
@@ -2,18 +2,13 @@
 
 namespace Alahaxe\HealthCheckBundle\Checks\Redis;
 
-use _PHPStan_71572f9a1\Psr\Log\LoggerAwareTrait;
 use Alahaxe\HealthCheck\Contracts\CheckInterface;
 use Alahaxe\HealthCheck\Contracts\CheckStatus;
 use Alahaxe\HealthCheck\Contracts\CheckStatusInterface;
-use Psr\Cache\CacheException;
-use Psr\Log\LoggerAwareInterface;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
 
-class RedisCheck implements CheckInterface, LoggerAwareInterface
+class RedisCheck implements CheckInterface
 {
-    use LoggerAwareTrait;
-    
     public function __construct(
         protected string $dsn,
         protected string $name = 'redis'
@@ -28,7 +23,7 @@ class RedisCheck implements CheckInterface, LoggerAwareInterface
         } catch (\Throwable $exception) {
             $status = CheckStatus::STATUS_INCIDENT;
         }
-        
+
         return new CheckStatus(
             $this->name,
             __CLASS__,
